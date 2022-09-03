83f88d1bd7fa5b5cd6a97f25ecd0c430310e9ef3
dannguyen <hieudankt@gmail.com>
2022-09-03 16:18:03 +0700

    trigger git action

--- backend/src/main/java/com/example/backend/controller.java
+++ backend/src/main/java/com/example/backend/controller.java
@@ -1,12 +1,4 @@
 package com.example.backend;
 
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("")
 public class controller {
-    @GetMapping("/")
-    public String getBook() {
-        return "hello this is my test";
-    }
 }
