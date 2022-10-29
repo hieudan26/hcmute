21fd3c8529e432c985e0b1710879ab36457573e7
Thang Duong <thangduong@futurify.vn>
2022-10-29 20:54:58 +0700

    feat: LUM-22: update route 404 for dynamic route

--- frontend/components/layouts/SetLayout.tsx
+++ frontend/components/layouts/SetLayout.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import cookie from 'react-cookies';
 import { login } from '../../app/slices/authSlice';
 import { RoleConstants } from '../../constants/roles.constant';
@@ -15,23 +15,19 @@ import UserLayout from './user/UserLayout.layout';
 export default function SetLayout({ children }: any) {
   const router = useRouter();
   const dispatch = useAppDispatch();
+  const [errorRoute, setErrorRoute] = useState<boolean>(false);
   const isLoggedIn = cookie.load(CookieConstants.IS_LOGGED_IN) ? true : false;
   const role = cookie.load(CookieConstants.ROLE) ? cookie.load(CookieConstants.ROLE) : RoleConstants.ANONYMOUS;
   const is_first_login = cookie.load(CookieConstants.IS_FIRST_LOGIN) ? cookie.load(CookieConstants.IS_FIRST_LOGIN) : 'false';
   const auth = useAppSelector((state) => state.auth.value);
 
   useEffect(() => {
-    // const userId = LocalUtils.getLocalStorage(LocalStorageConstants.USER_ID);
-    // const getInformationUser = async () => {
-    //   if (auth === null && userId) {
-    //     const response = await userService.getUserById(userId);
-    //     dispatch(login(response?.data));
-    //   }
-    // };
-    // getInformationUser();
-  }, []);
+    if (router.pathname === '/404') {
+      setErrorRoute(true);
+    }
+  }, [router.pathname]);
 
-  if (router.pathname === '/_error') {
+  if (router.pathname === '/_error' || errorRoute) {
     return <>{children}</>;
   } else if (router.pathname === '/login' || router.pathname === '/register' || router.pathname === '/forgot-password') {
     return <AuthLayout>{children}</AuthLayout>;
