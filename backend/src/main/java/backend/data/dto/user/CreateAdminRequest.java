1a47d2992da5be796f22f6bf2e93ea8dbcf56b4d
dannguyen <hieudankt@gmail.com>
2022-12-04 16:45:48 +0700

    fix  area province

--- backend/src/main/java/backend/data/dto/user/CreateAdminRequest.java
+++ backend/src/main/java/backend/data/dto/user/CreateAdminRequest.java
@@ -1,5 +1,14 @@
 package backend.data.dto.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateAdminRequest extends UserFirstLoginRequest{
     String id;
 }
