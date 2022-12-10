9a77c51a7d4f8c59aff3353dcee39f6657f9771f
dannguyen <hieudankt@gmail.com>
2022-12-10 22:07:05 +0700

    feat: LUM-31: search hashTag

---
+++ backend/src/main/java/backend/data/dto/hashtag/HashTagResponseWithLabel.java
@@ -0,0 +1,16 @@
+package backend.data.dto.hashtag;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HashTagResponseWithLabel {
+    String value;
+    String label;
+    Integer placeId;
+}
---
+++ backend/src/main/java/backend/data/dto/hashtag/QueryImageHashTagsParams.java
@@ -0,0 +1,15 @@
+package backend.data.dto.hashtag;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class QueryImageHashTagsParams {
+    String type;
+    String hashtag;
+}
