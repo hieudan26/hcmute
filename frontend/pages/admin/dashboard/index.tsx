93b8afbaf92f7e4c4d3f0807525e32582ef109b3
ThanggDuongg <thangduc.duong14@gmail.com>
2023-03-12 14:20:42 +0700

    feat: LUM-63: update grafana

--- frontend/pages/admin/dashboard/index.tsx
+++ frontend/pages/admin/dashboard/index.tsx
@@ -1,34 +1,20 @@
-import { AspectRatio, Box, Center, Button } from '@chakra-ui/react';
+import { AspectRatio, useColorMode } from '@chakra-ui/react';
 import { GetStaticProps, NextPage } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 export interface IAdminDashboardProps {}
 
 const AdminDashboard: NextPage = (props: IAdminDashboardProps) => {
-  const openGrafana = () => {
-    const newWindow = window.open(
-      'https://lumiere.grafana.net/d/fvuRtcFVz/lumiere?orgId=1&from=1670078065727&to=1670078365727',
-      '_blank',
-      'noopener,noreferrer'
-    );
-    if (newWindow) newWindow.opener = null;
-  };
+  const { colorMode } = useColorMode();
 
   return (
-    <Box w='full'>
-      <Center mb='8'>
-        <Button w='50%' onClick={openGrafana}>
-          Click to view detail
-        </Button>
-      </Center>
-      <AspectRatio maxW='full' ratio={1}>
-        <iframe
-          src='https://lumiere.grafana.net/d/fvuRtcFVz/lumiere?orgId=1&from=1670078268205&to=1670078303627'
-          allowFullScreen
-          // frameborder='0'
-        />
-      </AspectRatio>
-    </Box>
+    <AspectRatio w='full' maxW='full' ratio={1}>
+      <iframe
+        src={`http://18.142.192.152:3000/d/fvuRtcFVz/lumiere?orgId=1&from=1678604695432&to=1678604995432&theme=${colorMode}&kiosk=tv`} //=tv
+        // src={`http://18.142.192.152:3000/d/fvuRtcFVz/lumiere?orgId=1&from=1678604695432&to=1678604995432&theme=${colorMode}&kiosk`}
+        allowFullScreen
+      />
+    </AspectRatio>
   );
 };
 
